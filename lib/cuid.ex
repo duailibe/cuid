0df6212dc7b2acd9cb57ddcdb92707b0d096949a
Lucas Duailibe <lucasds@gmail.com>
2015-05-03 14:32:59 -0300

    using GenServer + docs

--- lib/cuid.ex
+++ lib/cuid.ex
@@ -1,47 +1,90 @@
 defmodule Cuid do
-  @moduledoc """
-  Collision-resistant ids.
-  """
+  use GenServer
+
   @block_size 4
   @base 36
-  @discrete_values 1_679_615
+  @discrete_values 1_679_616
 
-  def new do
-    "c" <> timestamp <> counter <> fingerprint <> random_block <> random_block
+  @moduledoc """
+  Collision-resistant ids.
+
+  Usage:
+
+    # Start the generator
+    {:ok, generator} = Cuid.start_link
+
+    # Generate a new CUID
+    Cuid.generate(generator)
+  """
+
+  @doc """
+  Starts a new generator.
+
+  Returns either `{:ok, pid}` or `{:error, reason}`
+  """
+  @spec start_link(process_opts :: list() | nil) :: {:ok, pid} | {:error, term}
+  def start_link(process_opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, process_opts)
   end
 
-  # TODO: use Agent to keep state
-  defp counter do
-    format 0
+  @doc """
+  Generates and returns a new CUID.
+  """
+  @spec generate(generator :: pid) :: String.t
+  def generate(generator) do
+    GenServer.call(generator, :generate)
   end
 
-  defp timestamp do
-    {mega, uni, micro} = :os.timestamp
-    (mega * 1_000_000 + uni) * 1_000 + div(micro, 100)
-    |> format(@block_size * 2)
+  ### Server callbacks
+
+  def init(:ok) do
+    {:ok, %{:fingerprint => get_fingerprint, :count => 0}}
   end
 
-  defp random_block do
-    :random.uniform(@discrete_values)
-    |> format
+  def handle_call(:generate, _, %{:fingerprint => fingerprint, :count => count} = state) do
+    cuid = Enum.join([
+      "c", timestamp, counter(count), fingerprint, random_block, random_block
+    ]) |> String.downcase
+
+    {:reply, cuid, %{state | :count => count + 1}}
   end
 
-  defp format(num, size \\ @block_size) do
+  # Returns the current count number as a 4-digit base-36 string
+  @spec counter(num :: number) :: String.t
+  defp counter(num) do
     num
     |> Integer.to_string(@base)
-    |> String.rjust(size, ?0)
-    |> String.slice(-size..-1)
-    |> String.downcase
+    |> String.rjust(@block_size, ?0)
   end
 
-  defp fingerprint do
-    padding = 2
+  # Returns the time as a 4-digit base-36 string
+  @spec timestamp() :: String.t
+  defp timestamp do
+    {mega, uni, micro} = :os.timestamp
+    rem((mega * 1_000_000 + uni) * 1_000_000 + micro, @discrete_values * @discrete_values)
+    |> Integer.to_string @base
+  end
 
-    pid = String.to_integer(System.get_pid)
+  # Returns a random 4-digit base-36 string
+  @spec random_block() :: String.t
+  defp random_block do
+    :random.uniform(@discrete_values - 1)
+    |> Integer.to_string(@base)
+    |> String.rjust(@block_size, ?0)
+  end
+
+  @operator @base * @base
+
+  # Returns the fingerprint of the host as a 4-digit base-36 string
+  # It consists of the current OS process ID and the hostname
+  @spec get_fingerprint() :: String.t
+  defp get_fingerprint do
+    pid = rem(String.to_integer(System.get_pid), @operator) * @operator
 
     hostname = to_char_list :net_adm.localhost
-    hostid = Enum.sum(hostname) + Enum.count(hostname) + @base
+    hostid = rem(Enum.sum(hostname) + Enum.count(hostname) + @base, @operator)
 
-    format(pid, padding) <> format(hostid, padding)
+    pid + hostid
+    |> Integer.to_string(@base)
   end
 end
--- test/cuid_test.exs
+++ test/cuid_test.exs
@@ -1,22 +1,27 @@
 defmodule CuidTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   require Cuid
 
-  test "generate string" do
-    assert is_binary Cuid.new
+  setup do
+    {:ok, generator} = Cuid.start_link
+    {:ok, generator: generator}
   end
 
-  test "format" do
-    c = Cuid.new
+  test "generate string", %{generator: generator} do
+    assert is_binary Cuid.generate(generator)
+  end
+
+  test "format", %{generator: generator} do
+    c = Cuid.generate(generator)
     assert String.length(c) == 25
     assert String.starts_with?(c, "c")
   end
 
-  test "collision" do
-    number_of_iterations = 50_000
+  test "collision", %{generator: generator} do
+    number_of_iterations = 200_000
 
     result =
-      Stream.repeatedly(&Cuid.new/0)
+      Stream.repeatedly(fn -> Cuid.generate(generator) end)
       |> Stream.take(number_of_iterations)
       |> Enum.into(HashSet.new)
 
