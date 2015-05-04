bdc076dc56923b1d0bbdc972c23340a9bcfee1d6
Lucas Duailibe <lucasds@gmail.com>
2015-05-04 01:48:00 -0300

    reorganizing

--- lib/cuid.ex
+++ lib/cuid.ex
@@ -1,10 +1,4 @@
 defmodule Cuid do
-  use GenServer
-
-  @block_size 4
-  @base 36
-  @discrete_values 1_679_616
-
   @moduledoc """
   Collision-resistant ids.
 
@@ -18,16 +12,6 @@ defmodule Cuid do
   """
 
   @doc """
-  Starts a new generator.
-
-  Returns either `{:ok, pid}` or `{:error, reason}`
-  """
-  @spec start_link(process_opts :: list() | nil) :: {:ok, pid} | {:error, term}
-  def start_link(process_opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, process_opts)
-  end
-
-  @doc """
   Generates and returns a new CUID.
   """
   @spec generate(generator :: pid) :: String.t
@@ -35,7 +19,16 @@ defmodule Cuid do
     GenServer.call(generator, :generate)
   end
 
-  ### Server callbacks
+  use GenServer
+
+  @doc """
+  Starts a new generator.
+  """
+  def start_link(process_opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, process_opts)
+  end
+
+  ## Server callbacks
 
   def init(:ok) do
     {:ok, %{:fingerprint => get_fingerprint, :count => 0}}
@@ -43,30 +36,31 @@ defmodule Cuid do
 
   def handle_call(:generate, _, %{:fingerprint => fingerprint, :count => count} = state) do
     cuid = Enum.join([
-      "c", timestamp, counter(count), fingerprint, random_block, random_block
+      "c", timestamp, format_counter(count), fingerprint, random_block, random_block
     ]) |> String.downcase
 
     {:reply, cuid, %{state | :count => count + 1}}
   end
 
-  # Returns the current count number as a 4-digit base-36 string
-  @spec counter(num :: number) :: String.t
-  defp counter(num) do
+  ## Helpers
+
+  @block_size 4
+  @base 36
+
+  defp format_counter(num) do
     num
     |> Integer.to_string(@base)
     |> String.rjust(@block_size, ?0)
   end
 
-  # Returns the time as a 4-digit base-36 string
-  @spec timestamp() :: String.t
+  @discrete_values 1_679_616
+
   defp timestamp do
     {mega, uni, micro} = :os.timestamp
     rem((mega * 1_000_000 + uni) * 1_000_000 + micro, @discrete_values * @discrete_values)
     |> Integer.to_string @base
   end
 
-  # Returns a random 4-digit base-36 string
-  @spec random_block() :: String.t
   defp random_block do
     :random.uniform(@discrete_values - 1)
     |> Integer.to_string(@base)
@@ -75,9 +69,6 @@ defmodule Cuid do
 
   @operator @base * @base
 
-  # Returns the fingerprint of the host as a 4-digit base-36 string
-  # It consists of the current OS process ID and the hostname
-  @spec get_fingerprint() :: String.t
   defp get_fingerprint do
     pid = rem(String.to_integer(System.get_pid), @operator) * @operator
 
